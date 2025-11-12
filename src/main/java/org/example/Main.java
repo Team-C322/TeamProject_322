93b493f28082398eb055f482e1457185e922ae89
Тимофей Патрушев <hyper_2007@mail.ru>
2025-11-12 14:36:05 +0300

    Добавлен класс Triangle и класс TriangleTest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 import allclasses.karachebanDI.StudyDesk;
+import allclasses.patrushev.Triangle;
 
 //TIP <h2> ДЛЯ ИСПОЛЬЗОВАНИЯ СВОЕГО КЛАССА ИМПОРТИРУЙТЕ ЕГО </h2>
 // Пример импорта выше
@@ -10,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         karachebanDI();
+        patrushevClassShowCase();
 
     }
     //region Методы студентов
@@ -25,6 +27,20 @@ public class Main {
         desk.ShowPrice("USD");
         System.out.println("Для покупки свяжитесь со мной");
         desk.ContactToBuy();
+    }
+
+    private static void patrushevClassShowCase() {
+        Triangle triangle = new Triangle(3, 4, 5);
+
+        System.out.println("*********************************");
+        System.out.println("Характеристики треугольника");
+        System.out.println(triangle.toString());
+        System.out.println("Периметр треугольника:");
+        System.out.println("P = " + triangle.getPerimeter());
+        System.out.println("Площадь треугольника:");
+        System.out.println("S = " + triangle.getArea());
+        System.out.println("Для использования свяжитесь со мной");
+        System.out.println("Автор: Патрушев");
     }
     //endregion
 }
