90acea0f32cf329780970de8fd2f2ab30d9f49d0
mitran0 <lasarcwg@mail.ru>
2025-11-15 12:29:23 +0300

    класс и тесты

---
+++ src/main/java/allclasses/Statsyuk/Person.java
@@ -0,0 +1,31 @@
+
+package allclasses.Statsyuk;
+
+public class Person {
+    private String name;
+    private int age;
+    private String city;
+
+    public Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void celebrateBirthday() {
+        this.age++;
+        System.out.println(name + " отметил день рождения и теперь ему " + age + " лет!");
+    }
+
+    public String getInfo() {
+        return "Имя: " + name + ", Возраст: " + age + ", Город: " + city;
+    }
+}
