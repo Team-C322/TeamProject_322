674e4ec9a69599f3c6b36393a70a0af562f9601b
Твоё Имя <linx9952@gmail.com>
2025-11-18 17:19:36 +0300

    PiggyBank ot kotelnikova

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import allclasses.bartasevich.generators.OrderGenerator;
 import allclasses.bartasevich.model.Courier;
 import allclasses.bartasevich.model.Order;
 import allclasses.karachebanDI.StudyDesk;
+import allclasses.kotelnikov.PiggyBank;
 
 //TIP <h2> ДЛЯ ИСПОЛЬЗОВАНИЯ СВОЕГО КЛАССА ИМПОРТИРУЙТЕ ЕГО </h2>
 // Пример импорта выше
@@ -15,7 +16,7 @@ public class Main {
 
         karachebanDI();
         bartasevich();
-
+        kotelnikov();
     }
     //region Методы студентов
     // Пример оформления смотрите тут
@@ -38,6 +39,19 @@ public class Main {
         System.out.println("\n***************** Бартасевич И. Г. *********************");
         System.out.println("\nИнформация о заказе:");
         System.out.println(order.toString());
+    }
+    private static void kotelnikov(){
+        System.out.println("\n***************** Котельников *********************");
+        PiggyBank myPiggyBank = new PiggyBank("Egor");
+        System.out.println("Тестируем копилку");
+        myPiggyBank.addMoney(1000.0);
+        myPiggyBank.addMoney(500.50);
+        myPiggyBank.takeMoney(300.0);
+        myPiggyBank.printInfo();
+        System.out.println("\nПроверка обработки ошибок");
+        myPiggyBank.addMoney(-100.0);
+        myPiggyBank.takeMoney(2000.0);
+        System.out.println("\nФинальный баланс: $" + myPiggyBank.getBalance() + ".");
     }
     //endregion
 }
